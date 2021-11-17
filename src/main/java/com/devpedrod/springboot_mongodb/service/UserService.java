2f62867e63420887dfe327f00c261cb64f8ceed3
dev-Pedrod <pedroolivxira@gmail.com>
2021-11-17 13:39:54 -0300

    Obtendo um usuário por id

--- src/main/java/com/devpedrod/springboot_mongodb/service/UserService.java
+++ src/main/java/com/devpedrod/springboot_mongodb/service/UserService.java
@@ -1,6 +1,7 @@
 package com.devpedrod.springboot_mongodb.service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Service;
 import com.devpedrod.springboot_mongodb.domain.User;
 import com.devpedrod.springboot_mongodb.dto.UserDTO;
 import com.devpedrod.springboot_mongodb.repository.UserRepository;
+import com.devpedrod.springboot_mongodb.service.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -19,6 +21,11 @@ public class UserService {
 	public List<UserDTO> findAll(){
 		List<User> list = repo.findAll();
 		return list.stream().map(obj -> new UserDTO(obj)).collect(Collectors.toList());
+	}
+	
+	public User findById(String id) {
+		Optional<User> user = repo.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 
 }
