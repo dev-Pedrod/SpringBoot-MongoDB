a2aae179459ec19ad7bab2249257f59d2429efaf
dev-Pedrod <pedroolivxira@gmail.com>
2021-11-22 18:01:15 -0300

    Endpoint para retornar os posts de um usuario

--- src/main/java/com/devpedrod/springboot_mongodb/resource/UserResource.java
+++ src/main/java/com/devpedrod/springboot_mongodb/resource/UserResource.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.devpedrod.springboot_mongodb.domain.Post;
 import com.devpedrod.springboot_mongodb.domain.User;
 import com.devpedrod.springboot_mongodb.dto.UserDTO;
 import com.devpedrod.springboot_mongodb.service.UserService;
@@ -58,5 +59,11 @@ public class UserResource {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@GetMapping(value = "/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 }
