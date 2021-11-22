bbada5121cadf11694020aa2a1438c6e34da6177
dev-Pedrod <pedroolivxira@gmail.com>
2021-11-22 18:19:06 -0300

    Obtendo um post por id

---
+++ src/main/java/com/devpedrod/springboot_mongodb/resource/PostResource.java
@@ -0,0 +1,25 @@
+package com.devpedrod.springboot_mongodb.resource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devpedrod.springboot_mongodb.domain.Post;
+import com.devpedrod.springboot_mongodb.service.PostService;
+
+@RestController
+@RequestMapping(value = "posts")
+public class PostResource {
+
+	@Autowired
+	private PostService service;
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Post> findById(@PathVariable String id) {
+		Post post = service.findById(id);
+		return ResponseEntity.ok().body(post);
+	}
+}
---
+++ src/main/java/com/devpedrod/springboot_mongodb/service/PostService.java
@@ -0,0 +1,23 @@
+package com.devpedrod.springboot_mongodb.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devpedrod.springboot_mongodb.domain.Post;
+import com.devpedrod.springboot_mongodb.repository.PostRepository;
+import com.devpedrod.springboot_mongodb.service.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository postRepository;
+	
+	public Post findById(String id) {
+		Optional<Post> post = postRepository.findById(id);
+		return post.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		
+	}
+}
