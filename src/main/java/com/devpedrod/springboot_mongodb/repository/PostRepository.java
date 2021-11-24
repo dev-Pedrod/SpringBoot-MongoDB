7b4fe1cbd0ce1e499fe8b221eef6bb6246f7710b
dev-Pedrod <pedroolivxira@gmail.com>
2021-11-24 15:51:41 -0300

    Consulta simples com @Query

--- src/main/java/com/devpedrod/springboot_mongodb/repository/PostRepository.java
+++ src/main/java/com/devpedrod/springboot_mongodb/repository/PostRepository.java
@@ -3,12 +3,17 @@ package com.devpedrod.springboot_mongodb.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.devpedrod.springboot_mongodb.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
 
+	//Também pode ser feito assim:
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
