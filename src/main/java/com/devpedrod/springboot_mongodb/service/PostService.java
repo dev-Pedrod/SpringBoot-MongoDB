3ea325d2188696a0f7a6408c136a2a24eda994e0
dev-Pedrod <pedroolivxira@gmail.com>
2021-11-24 15:40:17 -0300

    Consulta simples com query methods

--- src/main/java/com/devpedrod/springboot_mongodb/service/PostService.java
+++ src/main/java/com/devpedrod/springboot_mongodb/service/PostService.java
@@ -1,5 +1,6 @@
 package com.devpedrod.springboot_mongodb.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,5 +20,9 @@ public class PostService {
 		Optional<Post> post = postRepository.findById(id);
 		return post.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 		
+	}
+	
+	public List<Post> findByTitle(String text){
+		return postRepository.findByTitleContainingIgnoreCase(text);
 	}
 }
